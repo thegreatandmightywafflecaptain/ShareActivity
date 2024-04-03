10085102ac9149ab9e080a5a1bf36a6d8a646f18
thegreatandmightywafflecaptain <sirmadametheknight@gmail.com>
2024-04-03 15:16:22 -0400

    Toast shows up when user tries to share empty text.

--- app/src/main/java/edu/temple/funwithintents/MainActivity.kt
+++ app/src/main/java/edu/temple/funwithintents/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.EditText
 import android.widget.ImageButton
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -16,6 +17,10 @@ class MainActivity : AppCompatActivity() {
 
         // When the user clicks this button, share the text if not empty
         findViewById<ImageButton>(R.id.shareImageButton).setOnClickListener {
+            if(editText.text.length <= 0){
+                Toast.makeText(this, "Text is empty.", Toast.LENGTH_LONG).show()
+                return@setOnClickListener;
+            }
             val sendIntent: Intent = Intent().apply {
                 action = Intent.ACTION_SEND
                 putExtra(Intent.EXTRA_TEXT, editText.text)
